dc8bbdb7373f4f8062bf3442d7e4ea477ba66c61
Unknown <dmedina032@west-mec.org>
2019-02-28 18:25:22 -0700

    I made some more changes to the JavaScript
    I added in more arrays and a for loop for the items.

--- tc_cart.js
+++ tc_cart.js
@@ -11,3 +11,12 @@
    Filename: tc_cart.js
 	
 */
+var orderTotal = 0;
+var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> </tr>";
+// This is the for loop 
+for (var i = 0;) {
+    "<tr> <td><img src='tc_item.png' alt='item' /></td>";
+    var cartHTML = "<td>description</td> <td>$price</td> <td>quantity</td>";
+}
+
+var itemCost =
--- tc_order.js
+++ tc_order.js
@@ -19,4 +19,4 @@
 var item = [10582, 23015, 41807, 10041];
 var itemDescription = " 1975 Green Bay Packers Football (signed), Item 10582,Tom Landry 1955 Football Card (unsigned), Item 2301, 1916 Army-Navy Game, Framed Photo (signed), Item 41807, Protective Card Sheets, Item 10041 "
 var itemPrice = [149.93, 89.98, 334.93, 22.67];
-var itemQty =
+var itemQty = [1, 1, 1, 4];

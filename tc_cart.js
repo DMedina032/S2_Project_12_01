446cab64e7aee10b880f822c60320ff42c7991ae
Unknown <dmedina032@west-mec.org>
2019-03-05 16:20:09 -0700

    Final commit/ submit of review
    I finished up my comments for the JavaScript and for the HTML- index file.

--- tc_cart.js
+++ tc_cart.js
@@ -17,20 +17,19 @@ var orderTotal = 0;
 var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>";
 // This is the for loop for the entires in the item array
 for (var i = 0; i <= 3; i++) {
-    // i declared the carHTML varible again this time for the value of the html code that is also concatinated to the varible.
+    // I declared the carHTML varible again this time for the value of the html code that is also concatinated to the varible.
     cartHTML += "<tr><td><img src='tc_" + item[i] + ".png' alt='item'/></td>";
-    // 
+    // I declared the varible catHTML again and gave it the value of the text string for the table data and conatinated that to the varbibles that go along with each symbloe or reprsentation.
     cartHTML += "<td>" + itemDescription[i] + "</td><td>$" + itemPrice[i] + "</td><td>" + itemQty[i] + "</td>";
-
-    //
+    //by using the var attribute, I was then able to declare the varible of item cost and multiplyed the price by the quantity.
     var itemCost = itemPrice[i] * itemQty[i];
-    //
+    //I declared the varible cartHTML again and gave it a value of the text string for the table data for the cost 
     cartHTML += "<td>$" + itemCost + "</td></tr>";
-    //
+    //I declared the varible orderTotal and gave it the value of the same varible along concatinating it to the itemCost varible
     orderTotal = orderTotal + itemCost;
 }
 
 cartHTML += "<tr><td colspan='4'>Subtotal</td><td>$" + orderTotal + "</td></tr></table>";
 
-//
+//By using the docusment.getElementById it was able to go into the html document of the index and get the Id "cart" and called it back the the cartHTML varible.
 document.getElementById("cart").innerHTML = cartHTML;

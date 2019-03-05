dc585508b126bb8cbfde8ac9327d3d72c2a49fd4
Unknown <dmedina032@west-mec.org>
2019-03-04 18:21:05 -0700

    I finished the Javascript
    I still need to add in comments

--- tc_cart.js
+++ tc_cart.js
@@ -11,12 +11,26 @@
    Filename: tc_cart.js
 	
 */
+// I declared the var attribut and gave it a varible of orderTotal and set the value of this to 0 to calulate a running total cost of the order initaly starting at 0.
 var orderTotal = 0;
-var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> </tr>";
-// This is the for loop 
-for (var i = 0;) {
-    "<tr> <td><img src='tc_item.png' alt='item' /></td>";
-    var cartHTML = "<td>description</td> <td>$price</td> <td>quantity</td>";
+// For the next var, I declared the varible cartHTML to contain the html code for the shopping cart that will then look like a table for the user.
+var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>";
+// This is the for loop for the entires in the item array
+for (var i = 0; i <= 3; i++) {
+    // i declared the carHTML varible again this time for the value of the html code that is also concatinated to the varible.
+    cartHTML += "<tr><td><img src='tc_" + item[i] + ".png' alt='item'/></td>";
+    // 
+    cartHTML += "<td>" + itemDescription[i] + "</td><td>$" + itemPrice[i] + "</td><td>" + itemQty[i] + "</td>";
+
+    //
+    var itemCost = itemPrice[i] * itemQty[i];
+    //
+    cartHTML += "<td>$" + itemCost + "</td></tr>";
+    //
+    orderTotal = orderTotal + itemCost;
 }
 
-var itemCost =
+cartHTML += "<tr><td colspan='4'>Subtotal</td><td>$" + orderTotal + "</td></tr></table>";
+
+//
+document.getElementById("cart").innerHTML = cartHTML;
--- tc_order.js
+++ tc_order.js
@@ -16,7 +16,11 @@
    The itemQty array contains the quantity ordered of each item
    
 */
+// I declared the varible item by using the var attribute creating an array for the item numbers. 
 var item = [10582, 23015, 41807, 10041];
-var itemDescription = " 1975 Green Bay Packers Football (signed), Item 10582,Tom Landry 1955 Football Card (unsigned), Item 2301, 1916 Army-Navy Game, Framed Photo (signed), Item 41807, Protective Card Sheets, Item 10041 "
+// ny using the var attribute, I declared the varible itemDescription and set the descriptions for the page in an string for the users to read.
+var itemDescription = ["1975 Green Bay Packers Football (signed), Item 10582", "Tom Landry 1955 Football Card (unsigned), Item 2301", "1916 Army-Navy Game, Framed Photo (signed), Item 41807", "Protective Card Sheets, Item 10041"];
+// I declared the itemPrice varible and set it to a value containing item prices. 
 var itemPrice = [149.93, 89.98, 334.93, 22.67];
+// I declared the varible itemQty by setting the var attribute for the customer orders as the value.
 var itemQty = [1, 1, 1, 4];
